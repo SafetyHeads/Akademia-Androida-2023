7893ed768d042e58e7b376f45aca08db34be0392
jordanrafalm <jordanrafalm@gmail.com>
2023-01-23 11:53:48 +0100

    SplashScreenActivity without font and colors

---
+++ app/src/main/java/com/safetyheads/akademiaandroida/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.safetyheads.akademiaandroida
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreenActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long = 3000 // 3 sec
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
