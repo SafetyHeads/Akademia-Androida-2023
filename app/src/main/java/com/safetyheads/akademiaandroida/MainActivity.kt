60850d83642942d53f734c725a0fb463ad3c0c6e
Rafal Krystek <rafal.krystek@outlook.com>
2023-01-24 16:17:56 +0100

    Created back arrow view

--- app/src/main/java/com/safetyheads/akademiaandroida/MainActivity.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<View>(R.id.back_arrow_view).setOnClickListener { Log.d("tag","Arrowclicked") }
+        findViewById<View>(R.id.back_arrow_view).setOnClickListener{ Log.d("tag","Arrowclicked") }
     }
 }
--- app/src/main/java/com/safetyheads/akademiaandroida/views/BackArrowView.java
+++ app/src/main/java/com/safetyheads/akademiaandroida/views/BackArrowView.java
@@ -3,10 +3,8 @@ package com.safetyheads.akademiaandroida.views;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-
 import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,19 +12,16 @@ import androidx.annotation.Nullable;
 import com.safetyheads.akademiaandroida.R;
 
 public class BackArrowView extends FrameLayout {
-
     private ImageView arrow;
 
     public BackArrowView(@NonNull Context context) {
         super(context);
         initView();
     }
-
     public BackArrowView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         initView();
     }
-
     public BackArrowView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         initView();
