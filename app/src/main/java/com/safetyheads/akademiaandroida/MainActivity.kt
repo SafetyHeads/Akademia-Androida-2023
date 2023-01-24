9ec294aeaf9052831d69ee5d76aabd0148029b77
Rafal Krystek <rafal.krystek@outlook.com>
2023-01-24 16:01:18 +0100

    Created back arrow view

--- app/src/main/java/com/safetyheads/akademiaandroida/MainActivity.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/MainActivity.kt
@@ -2,10 +2,13 @@ package com.safetyheads.akademiaandroida
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.back_arrow_view).setOnClickListener { Log.d("tag","Arrowclicked") }
     }
 }
---
+++ app/src/main/java/com/safetyheads/akademiaandroida/views/BackArrowView.java
@@ -0,0 +1,46 @@
+package com.safetyheads.akademiaandroida.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.safetyheads.akademiaandroida.R;
+
+public class BackArrowView extends FrameLayout {
+
+    private ImageView arrow;
+
+    public BackArrowView(@NonNull Context context) {
+        super(context);
+        initView();
+    }
+
+    public BackArrowView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    public BackArrowView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView();
+    }
+    private void initView(){
+        final View view = inflate(getContext(), R.layout.view_arrow,this);
+        arrow= findViewById(R.id.arrow);
+    }
+
+    public void setImage(@DrawableRes int imageRes){
+        arrow.setImageResource(imageRes);
+    }
+
+
+
+
+}
