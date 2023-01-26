05d8f9d5fe0941fb7f5658f4083007176fba97a4
Rafal Krystek <rafal.krystek@outlook.com>
2023-01-26 16:37:16 +0100

    Created back arrow view

--- app/src/main/java/com/safetyheads/akademiaandroida/MainActivity.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.safetyheads.akademiaandroida
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<View>(R.id.back_arrow_view).setOnClickListener{ Log.d("tag","Arrowclicked") }
+        findViewById<View>(R.id.back_arrow_view).setOnClickListener { Log.d("tag", "Arrowclicked") }
     }
 }
--- app/src/main/java/com/safetyheads/akademiaandroida/views/BackArrowView.java
+++ app/src/main/java/com/safetyheads/akademiaandroida/views/BackArrowView.java
@@ -3,39 +3,41 @@ package com.safetyheads.akademiaandroida.views;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
+
 import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.safetyheads.akademiaandroida.R;
 
-public class BackArrowView extends FrameLayout {
+public class BackArrowView extends ConstraintLayout {
     private ImageView arrow;
 
     public BackArrowView(@NonNull Context context) {
         super(context);
         initView();
     }
+
     public BackArrowView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         initView();
     }
+
     public BackArrowView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         initView();
     }
-    private void initView(){
-        final View view = inflate(getContext(), R.layout.view_arrow,this);
-        arrow= findViewById(R.id.arrow);
+
+    private void initView() {
+        final View view = inflate(getContext(), R.layout.view_arrow, this);
+        arrow = findViewById(R.id.arrow);
     }
 
-    public void setImage(@DrawableRes int imageRes){
+    public void setImage(@DrawableRes int imageRes) {
         arrow.setImageResource(imageRes);
     }
-
-
 
 
 }
