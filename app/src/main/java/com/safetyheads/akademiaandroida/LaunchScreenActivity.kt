122d420f79dbc2c2d0e9aebbb23324de2d6778c0
Kacper Lichwiarz <kapko1999@gmail.com>
2023-01-31 21:21:23 +0100

    fixed layout margins, added styles for textViews, changed AppCompatButton to Button, changed color of statusBarColor, removed ActionBar in themes, changed method of adding a footer

--- app/src/main/java/com/safetyheads/akademiaandroida/LaunchScreenActivity.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/LaunchScreenActivity.kt
@@ -13,17 +13,12 @@ class LaunchScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch_screen)
 
-        supportActionBar?.hide()
-
         val welcomeMessage = findViewById<TextView>(R.id.txt_welcome)
         val welcomeMessageTxT = welcomeMessage.text
 
-        val footer = Footer()
-        if(savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.footer, footer)
-                .commitAllowingStateLoss()
-        }
+        supportFragmentManager.beginTransaction()
+            .add(R.id.parent_layout, Footer())
+            .commit()
 
         val shColor = ContextCompat.getColor(this, R.color.p_60)
         val sfStart = welcomeMessageTxT.indexOf(resources.getString(R.string.launch_screen_txt_company_name))
