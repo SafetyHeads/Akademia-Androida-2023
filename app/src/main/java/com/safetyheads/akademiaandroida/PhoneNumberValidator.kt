b8872cc0778996f26d6164f922b4d7882d850292
magda2307 <magdalenasokolowska2307@gmail.com>
2023-02-02 17:13:36 +0100

    use lambda function in validator instead of creating an object that implements the TextWatcher

--- app/src/main/java/com/safetyheads/akademiaandroida/PhoneNumberValidator.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/PhoneNumberValidator.kt
@@ -1,23 +1,16 @@
 package com.safetyheads.akademiaandroida
 
 import android.content.Context
-import android.text.Editable
-import android.text.TextWatcher
 import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
 
 object PhoneNumberValidator {
     private val phoneNumberRegex = "^(\\+\\d{2}[- ]?)?\\d{9}\$".toRegex()
 
     fun attach(editText: EditText, context: Context) {
-        editText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                validatePhoneNumber(s.toString(), editText, context)
-            }
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                validatePhoneNumber(s.toString(), editText, context)
-            }
-        })
+        editText.addTextChangedListener {
+            validatePhoneNumber(it.toString(), editText, context)
+        }
     }
 
     private fun isValid(phoneNumber: String): Boolean {
