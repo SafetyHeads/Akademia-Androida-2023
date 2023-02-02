c7fe9327ffd547c4a2dcb17740f9fff9b755f1f8
Zalezny <zaleznypl@gmail.com>
2023-02-02 10:49:48 +0100

    short instruction on the top file, removed validateFullName and created two other functions (validateName, validateSurname). Deleted spaces

--- app/src/main/java/com/safetyheads/akademiaandroida/utils/FullNameVaildator.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/utils/FullNameVaildator.kt
@@ -6,66 +6,62 @@ import android.widget.EditText
 import androidx.core.widget.addTextChangedListener
 import com.safetyheads.akademiaandroida.R
 
+/*
+* To use this validator you have to add to your code:
+* FullNameValidator.attach(fullNameEditText, this)
+*/
 
 object FullNameValidator {
 
     private const val maxTextLength = 10
     private const val minNameAndSurnameLength = 2
-
     fun attach(editText: EditText, context: Context) {
-
         val regexFilter =
             InputFilter { source, start, end, _, _, _ ->
                 for (i in start until end) {
-
                     if (!Character.isLetter(source[i]) && source[i] != ' ') {
                         return@InputFilter source.dropLast(1)
                     }
                 }
                 null
             }
-
         val lengthFilter = InputFilter.LengthFilter(maxTextLength)
-
         editText.filters = arrayOf<InputFilter>(lengthFilter, regexFilter)
-
-        editText.addTextChangedListener {
-
-            validateFullName(it.toString(), editText, context)
-
+        editText.addTextChangedListener { text ->
+            val splitNames = text.toString().split("\\s".toRegex())
+            val spacesAmount = text.toString().count { it == ' ' }
+            if (validateName(splitNames.first(), splitNames, editText, context) &&
+                validateSurname(splitNames.last(), splitNames, spacesAmount, editText, context)
+            )
+                editText.error = null
         }
     }
 
-    private fun validateFullName(fullName: String, editText: EditText, context: Context) {
-        val splitNames = fullName.split("\\s".toRegex())
-
-        val spacesAmount = fullName.count { it == ' ' }
-
-        // name
-        if (splitNames.first().length < minNameAndSurnameLength && splitNames.first()
-                .isNotEmpty()
-        ) {
+    private fun validateName(
+        name: String, splitNames: List<String>, editText: EditText,
+        context: Context
+    ): Boolean {
+        if (name.length < minNameAndSurnameLength && splitNames.first().isNotEmpty()) {
             editText.error = context.getString(R.string.invalid_name_message)
-            return
-
-        }
-        //surname
-        else if (splitNames.last().length < minNameAndSurnameLength && splitNames.size >= 2 && spacesAmount == 1) {
-            editText.error = context.getString(R.string.invalid_surname_message)
-            return
-
-        } else if (fullName.isEmpty()) {
+            return false
+        } else if (name.isEmpty()) {
             editText.error = context.getString(R.string.invalid_empty_message)
-            return
+            return false
+        }
+        return true
+    }
 
+    private fun validateSurname(
+        surname: String, splitNames: List<String>, spacesAmount: Int,
+        editText: EditText, context: Context
+    ): Boolean {
+        if (surname.length < minNameAndSurnameLength && splitNames.size >= 2 && spacesAmount == 1) {
+            editText.error = context.getString(R.string.invalid_surname_message)
+            return false
         } else if (spacesAmount > 1) {
             editText.error = context.getString(R.string.invalid_spaces_message)
-        } else {
-            editText.error = null
-            return
-
+            return false
         }
+        return true
     }
-
-
 }
