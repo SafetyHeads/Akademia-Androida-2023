d470ab929c89d7b41cbbd582ce0abe4012ba074c
jordanrafalm <jordanrafalm@gmail.com>
2023-02-08 19:11:12 +0100

    add instruction

--- app/src/main/java/com/safetyheads/akademiaandroida/PasswordValidator.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/PasswordValidator.kt
@@ -4,7 +4,12 @@ import android.content.Context
 import android.widget.EditText
 import androidx.core.widget.addTextChangedListener
 
+/*
+To use this validator you have to add to your code:
 
+      val editTextPassword = findViewById<EditText>(R.id.edit)
+        PasswordValidator.attach(editTextPassword, this)
+ */
 class PasswordValidator {
     private val PasswordRegex =
         Regex("[a-zA-Z0-9][a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]{11,}$")
@@ -26,5 +31,4 @@ class PasswordValidator {
     private fun isValid(password: String): Boolean {
         return PasswordRegex.matches(password)
     }
-
 }
