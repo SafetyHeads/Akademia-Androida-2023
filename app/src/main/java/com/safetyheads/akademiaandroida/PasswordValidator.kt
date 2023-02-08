5c5036da88025f8cadca766f1a16a1cba31052c3
jordanrafalm <jordanrafalm@gmail.com>
2023-02-08 18:34:41 +0100

    PasswordValidator version 2

--- app/src/main/java/com/safetyheads/akademiaandroida/PasswordValidator.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/PasswordValidator.kt
@@ -1,24 +1,30 @@
 package com.safetyheads.akademiaandroida
 
-import android.text.InputFilter
-import android.text.Spanned
-import android.widget.Toast
+import android.content.Context
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
 
 
-class PasswordValidator : InputFilter {
-    override fun filter(
-        source: CharSequence,
-        start: Int,
-        end: Int,
-        dest: Spanned,
-        dstart: Int,
-        dend: Int
-    ): CharSequence? {
-        val password = dest.toString() + source.toString()
-        return if (password.matches(PASSWORD_REGEX.toRegex())) null else ""
+class PasswordValidator {
+    private val PasswordRegex =
+        Regex("[a-zA-Z0-9][a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]{11,}$")
+
+    fun attach(editText: EditText, context: Context) {
+        editText.addTextChangedListener {
+            validatePassword(it.toString(), editText, context)
+        }
     }
 
-    companion object {
-        private const val PASSWORD_REGEX = "\"^[a-zA-Z0-9]\\\\w{11,}\$\"$"
+    private fun validatePassword(password: String, editText: EditText, context: Context) {
+        if (!isValid(password)) {
+            editText.error = context.getString(R.string.invalid_password)
+        } else {
+            editText.error = null
+        }
     }
+
+    private fun isValid(password: String): Boolean {
+        return PasswordRegex.matches(password)
+    }
+
 }
