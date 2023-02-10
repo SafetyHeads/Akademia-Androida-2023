f4836fc57100d2bb1e591972a2c46c134ef6b811
Rafał Zaborowski <raaf1981@gmail.com>
2023-02-10 13:53:00 +0100

    contact us screen added

---
+++ app/src/main/java/com/safetyheads/ContactUsFragment.kt
@@ -0,0 +1,18 @@
+package com.safetyheads
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.safetyheads.akademiaandroida.R
+
+class ContactUsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_contact_us, container, false)
+    }
+}
