5d5aec3cfab12bb081b864bf77bb95ef061676f5
Zalezny <zaleznypl@gmail.com>
2023-02-12 12:40:37 +0100

    changed: created more readable code

--- app/src/main/java/com/safetyheads/akademiaandroida/utils/FullNameVaildator.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/utils/FullNameVaildator.kt
@@ -13,8 +13,9 @@ import com.safetyheads.akademiaandroida.R
 
 object FullNameValidator {
 
-    private const val maxTextLength = 100
-    private const val minNameAndSurnameLength = 2
+    private const val MAX_TEXT_LENGTH = 100
+    private const val MIN_NAME_AND_SURNAME_LENGTH = 2
+    private const val MIN_SPACE_AMOUNT = 1
     fun attach(editText: EditText, context: Context) {
         val regexFilter =
             InputFilter { source, start, end, _, _, _ ->
@@ -25,11 +26,14 @@ object FullNameValidator {
                 }
                 null
             }
-        val lengthFilter = InputFilter.LengthFilter(maxTextLength)
+        val lengthFilter = InputFilter.LengthFilter(MAX_TEXT_LENGTH)
         val bigOnlyFirstLetterFilter = InputFilter { source, start, end, _, _, _ ->
             var upperCase = 0
             for (i in start until end) {
-                if (Character.isUpperCase(source[i]) && source[i] != ' ' && upperCase > 1)
+                if (Character.isUpperCase(source[i]) && checkIsNotSpace(source[i]) && isNotFirstChar(
+                        upperCase
+                    )
+                )
                     return@InputFilter source.dropLast(1)
                 upperCase++
             }
@@ -39,7 +43,7 @@ object FullNameValidator {
         editText.addTextChangedListener { text ->
             val splitNames = text.toString().split("\\s".toRegex())
             val spacesAmount = text.toString().count { it == ' ' }
-            val surname = if (splitNames.size > 1) splitNames[1] else ""
+            val surname = splitNames.getOrNull(1).orEmpty()
             if (validateName(splitNames.first(), splitNames, editText, context) &&
                 validateSurname(surname, splitNames, spacesAmount, editText, context)
             )
@@ -47,17 +51,36 @@ object FullNameValidator {
         }
     }
 
+    private fun checkIsNotSpace(source: Char): Boolean {
+        if (source.isWhitespace())
+            return false
+        return true
+    }
+
+    private fun isNotFirstChar(upperCase: Int): Boolean {
+        if (upperCase > 1)
+            return true
+        return false
+    }
+
+
+    private fun checkListSizeIsTwo(splitNames: List<String>): Boolean {
+        if (splitNames.size >= 2)
+            return true
+        return false
+    }
+
     private fun validateName(
         name: String, splitNames: List<String>, editText: EditText,
         context: Context
     ): Boolean {
-        if (name.length < minNameAndSurnameLength && splitNames.first().isNotEmpty()) {
+        if (name.length < MIN_NAME_AND_SURNAME_LENGTH && splitNames.first().isNotEmpty()) {
             editText.error = context.getString(R.string.invalid_name_message)
             return false
         } else if (name.isEmpty()) {
             editText.error = context.getString(R.string.invalid_empty_message)
             return false
-        } else if (!name[0].isUpperCase()) {
+        } else if (!name.first().isUpperCase()) {
             editText.error = context.getString(R.string.invalid_name_message)
             return false
         }
@@ -68,10 +91,12 @@ object FullNameValidator {
         surname: String, splitNames: List<String>, spacesAmount: Int,
         editText: EditText, context: Context
     ): Boolean {
-        if (surname.length < minNameAndSurnameLength && splitNames.size >= 2 && spacesAmount == 1) {
+        if (surname.length < MIN_NAME_AND_SURNAME_LENGTH && checkListSizeIsTwo(splitNames)
+            && spacesAmount == MIN_SPACE_AMOUNT
+        ) {
             editText.error = context.getString(R.string.invalid_surname_message)
             return false
-        } else if (spacesAmount > 1) {
+        } else if (spacesAmount > MIN_SPACE_AMOUNT) {
             editText.error = context.getString(R.string.invalid_spaces_message)
             return false
         } else if (surname.isEmpty()) {
