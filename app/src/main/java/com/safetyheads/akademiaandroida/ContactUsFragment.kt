46c90be47dc503197ede4896ce474cffc5744fe2
Rafał Zaborowski <raaf1981@gmail.com>
2023-02-14 22:32:19 +0100

    dimens granularity fixed, wrong package fixed

--- app/src/main/java/com/safetyheads/akademiaandroida/ContactUsFragment.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/ContactUsFragment.kt
@@ -1,11 +1,10 @@
-package com.safetyheads
+package com.safetyheads.akademiaandroida
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.safetyheads.akademiaandroida.R
 
 class ContactUsFragment : Fragment() {
 
