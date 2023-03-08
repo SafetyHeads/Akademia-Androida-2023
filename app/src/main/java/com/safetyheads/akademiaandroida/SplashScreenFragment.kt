909b0ac2d95e76a3b00b826ff1111a992ee4137d
Albert Bilski <albertbilskii@gmail.com>
2023-03-08 13:26:30 +0100

    Uncomment handler navigation

--- app/src/main/java/com/safetyheads/akademiaandroida/SplashScreenFragment.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/SplashScreenFragment.kt
@@ -1,10 +1,13 @@
 package com.safetyheads.akademiaandroida
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.safetyheads.akademiaandroida.databinding.FragmentSplashScreenBinding
 
 
@@ -22,10 +25,10 @@ class SplashScreenFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        Handler(Looper.myLooper()!!).postDelayed(
-//            {
-//                findNavController().navigate(R.id.action_splashScreen_to_launchScreen)
-//            }, 3000
-//        )
+        Handler(Looper.myLooper()!!).postDelayed(
+            {
+                findNavController().navigate(R.id.action_splashScreen_to_launchScreenFragment)
+            }, 3000
+        )
     }
 }
