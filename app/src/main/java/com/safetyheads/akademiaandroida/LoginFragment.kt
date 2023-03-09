a8db51588c5a09cd9f95d195c455b05eeff20f1b
infitex <michal.woj1011@gmail.com>
2023-03-09 13:56:06 +0100

    Task 33

--- app/src/main/java/com/safetyheads/akademiaandroida/LoginFragment.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/LoginFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.safetyheads.akademiaandroida.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
+    private lateinit var binding: FragmentLoginBinding
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
 
 }
