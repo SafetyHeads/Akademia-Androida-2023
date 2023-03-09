4a2db0d8cf80470264fcd3730bd951e9164c712b
infitex <michal.woj1011@gmail.com>
2023-03-09 14:34:15 +0100

    Task 61

---
+++ app/src/main/java/com/safetyheads/akademiaandroida/RegistrationFragment.kt
@@ -0,0 +1,24 @@
+package com.safetyheads.akademiaandroida
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.safetyheads.akademiaandroida.databinding.FragmentRegistrationBinding
+
+class RegistrationFragment : Fragment() {
+
+    private lateinit var binding: FragmentRegistrationBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRegistrationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
