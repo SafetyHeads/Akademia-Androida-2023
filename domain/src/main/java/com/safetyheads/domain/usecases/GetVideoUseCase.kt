26e03ba8de8e79dd2ed8089a635c9cad8b53d3b3
Kuba-Nalepa <nalepak11@gmail.com>
2023-03-29 02:48:13 +0200

    Correctly setting domain module

--- domain/src/main/java/com/safetyheads/domain/usecases/GetVideoUseCase.kt
+++ domain/src/main/java/com/safetyheads/domain/usecases/GetVideoUseCase.kt
@@ -1,7 +1,23 @@
 package com.safetyheads.domain.usecases
 
-class GetVideoUseCase() {
-    suspend fun invoke(videoId: String) {
+import com.safetyheads.domain.entities.Video
+import com.safetyheads.domain.repositories.VideoRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
+class GetVideoUseCase(private val repository: VideoRepository): BaseUseCase<GetVideoUseCase.VideoParam,Video> {
+
+    class VideoParam(val videoId: String): BaseUseCase.Params
+
+    override suspend fun invoke(parameter: VideoParam): Flow<Result<Video>> {
+        return flow {
+            try {
+                repository.getVideo(parameter.videoId).collect{ video ->
+                    emit(Result.success(video))
+                }
+            }   catch (error: Exception) {
+                emit(Result.failure(error))
+            }
+        }
     }
 }
