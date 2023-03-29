3fe907f69c8dcc5bc7fb1fdca4e03b82c97bbbe4
jordanrafalm <jordanrafalm@gmail.com>
2023-03-29 21:33:29 +0200

    1.change remote_config file from json to xml 2.create instance Remote Config to firebase 3.download data from firebase

--- app/src/main/java/com/safetyheads/akademiaandroida/MainActivity.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.safetyheads.akademiaandroida
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
-import android.view.ViewGroup
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import com.safetyheads.akademiaandroida.ActivitiesList.ListActivity
 import com.safetyheads.akademiaandroida.contactusform.ContactUsFragment
 import com.safetyheads.akademiaandroida.databinding.ActivityMainBinding
@@ -69,19 +73,44 @@ class MainActivity : AppCompatActivity() {
             .add(R.id.footer_container, Footer())
             .commit()
 
-        //testting firebase.crashlytics
+        //testing firebase.crashlytics
         val crashButton = Button(this)
         crashButton.text = "Test Crash"
         crashButton.setOnClickListener {
             throw RuntimeException("Test Crash") // Force a crash
         }
 
-        addContentView(
-            crashButton, ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-            )
-        )
+        //create instance Remote Config to firebase and download data
+        val remoteConfig = FirebaseRemoteConfig.getInstance()
+        val configSettings = FirebaseRemoteConfigSettings.Builder()
+            .setFetchTimeoutInSeconds(60)
+            .build()
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
+
+        val versionCode = remoteConfig.getString("versionCode")
+        val apiUrl = remoteConfig.getString("apiUrl")
+
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val updated = task.result
+                    Log.d(TAG, "Config params updated: $updated")
+
+                    Toast.makeText(
+                        this,
+                        "versionCode:" + versionCode +
+                                "apiUrl:" + apiUrl,
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                } else {
+                    Toast.makeText(
+                        this, "Fetch failed",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 
     private fun openFragment(fragment: Fragment) {
