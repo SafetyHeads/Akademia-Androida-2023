edfa215ddfefa1f389d87d7bc6b7ce0933f4adf3
infitex <michal.woj1011@gmail.com>
2023-04-06 10:16:48 +0200

    Task 61

--- app/src/main/java/com/safetyheads/akademiaandroida/sign_up/SignUpFragment.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/sign_up/SignUpFragment.kt
@@ -5,14 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.material.textfield.TextInputEditText
-import com.safetyheads.akademiaandroida.R
 import com.safetyheads.akademiaandroida.databinding.FragmentSignUpBinding
-import com.safetyheads.akademiaandroida.utils.FullNameValidator
+import com.safetyheads.akademiaandroida.utils.PasswordValidator
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SignUpFragment : Fragment() {
 
     private lateinit var binding: FragmentSignUpBinding
+    private val viewModel: SignUpViewModel by viewModel()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,7 +20,33 @@ class SignUpFragment : Fragment() {
     ): View {
         binding = FragmentSignUpBinding.inflate(inflater, container, false)
 
+        binding.buttonSignUp.setOnClickListener() {
+
+            //przypinanie powinno byc w onViewCreate
+            viewModel.signUp(binding.eTextFullName, binding.eTextEmailAddress, binding.eTextPassword, binding.eTextConfirmPassword)
+        }
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+
+        //Reszte validatorów podpiąć
+
+        //UserReposytory to interface w niej createUser podaje email i haslo
+
+        //ProgressBar dorobic powinien sie pojawiac i znikac na podstawie LiveDate
+
+        //Livedata Regisistration Complete
+
+        //ScrollView
+
+
+        PasswordValidator.attach(binding.eTextPassword, requireContext())
+
+
     }
 
 }
--- app/src/main/java/com/safetyheads/akademiaandroida/sign_up/SignUpViewModel.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/sign_up/SignUpViewModel.kt
@@ -1,8 +1,13 @@
 package com.safetyheads.akademiaandroida.sign_up
 
 import androidx.lifecycle.ViewModel
+import com.google.android.material.textfield.TextInputEditText
+import com.safetyheads.akademiaandroida.dropdownlist.LoadItemsToDropDownListUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 
-class SignUpViewModel : ViewModel() {
+class SignUpViewModel (private val signUpUseCase: SignUpUseCase) : ViewModel() {
+    fun signUp(eTextFullName: TextInputEditText, eTextEmailAddress: TextInputEditText, eTextPassword: TextInputEditText, eTextConfirmPassword: TextInputEditText) {
+
+    }
 
 }
