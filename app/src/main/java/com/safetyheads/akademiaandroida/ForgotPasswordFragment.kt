6144db48cc83ae26b344ab5556f86c497dbad21a
Zalezny <zaleznypl@gmail.com>
2023-04-07 16:16:51 +0200

    added ForgotPassword, work H2View.kt

---
+++ app/src/main/java/com/safetyheads/akademiaandroida/ForgotPasswordFragment.kt
@@ -0,0 +1,25 @@
+package com.safetyheads.akademiaandroida
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.safetyheads.akademiaandroida.databinding.FragmentForgotPasswordBinding
+
+class ForgotPasswordFragment : Fragment() {
+
+    private lateinit var binding: FragmentForgotPasswordBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentForgotPasswordBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/com/safetyheads/akademiaandroida/H2View.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/H2View.kt
@@ -3,6 +3,7 @@ package com.safetyheads.akademiaandroida
 import android.content.Context
 import android.content.res.TypedArray
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.widget.LinearLayout
 import com.safetyheads.akademiaandroida.databinding.ViewH2Binding
 
@@ -24,6 +25,7 @@ class H2View @JvmOverloads constructor(
     private lateinit var binding: ViewH2Binding
 
     init {
+        binding = ViewH2Binding.inflate(LayoutInflater.from(context), this)
         binding.textViewH2.text = attributes.getString(R.styleable.H2View_text)
     }
 }
