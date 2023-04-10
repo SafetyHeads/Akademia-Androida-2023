2fde64fce2a300dea5abfbba35688d1079763e87
infitex <michal.woj1011@gmail.com>
2023-04-10 18:48:03 +0200

    Task 61

--- app/src/main/java/com/safetyheads/akademiaandroida/sign_up/RegisterUseCase.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/sign_up/RegisterUseCase.kt
@@ -1,9 +1,23 @@
 package com.safetyheads.akademiaandroida.sign_up
 
+import com.safetyheads.domain.usecases.BaseUseCase
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class RegisterUseCase(
     private val repository: UserRepository
-) {
+) : BaseUseCase<RegisterUseCase.UserParams, User> {
+    class UserParams(val fullName: String, val email: String, val password: String): BaseUseCase.Params
+    override suspend fun invoke(parameter: RegisterUseCase.UserParams): Flow<Result<User>> {
 
+            return flow {
+                try {
+                    repository.createUser(parameter.fullName, parameter.email, parameter.password).collect{ user ->
+                        emit(Result.success(user))
+                    }
+                }   catch (error: Exception) {
+                    emit(Result.failure(error))
+                }
+            }
+    }
 }
--- app/src/main/java/com/safetyheads/akademiaandroida/sign_up/UserRepository.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/sign_up/UserRepository.kt
@@ -3,5 +3,5 @@ package com.safetyheads.akademiaandroida.sign_up
 import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
-    suspend fun createUser() : Flow<User>
+     fun createUser(fullName: String, email: String, password: String) : Flow<User>
 }
