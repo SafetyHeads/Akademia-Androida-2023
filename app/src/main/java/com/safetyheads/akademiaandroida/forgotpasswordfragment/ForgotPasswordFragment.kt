dfe08d1793bbe751c9d4509bc94e2393628ef505
Zalezny <zaleznypl@gmail.com>
2023-04-17 15:54:21 +0200

    added on failed dialog

--- app/src/main/java/com/safetyheads/akademiaandroida/forgotpasswordfragment/ForgotPasswordFragment.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/forgotpasswordfragment/ForgotPasswordFragment.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.safetyheads.akademiaandroida.R
 import com.safetyheads.akademiaandroida.databinding.FragmentForgotPasswordBinding
+import com.safetyheads.akademiaandroida.forgotpasswordfragment.snackbar.LoginSnackBar
 import com.safetyheads.akademiaandroida.utils.EmailValidator
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -53,7 +54,10 @@ class ForgotPasswordFragment : Fragment() {
                 findNavController()
                     .navigate(R.id.action_forgot_password_fragment_to_login_fragment, bundle)
             } else {
-                TODO("Failed of sent reset Password on email")
+                LoginSnackBar.make(
+                    binding.root,
+                    message = requireActivity().getString(R.string.failed_sent_forgot_password)
+                ).show()
             }
         }
 
