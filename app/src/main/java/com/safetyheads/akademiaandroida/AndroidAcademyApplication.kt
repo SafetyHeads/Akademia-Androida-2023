8d54d2742456f0a18eef64ae1a658cb3e64a6162
Albert Bilski <albertbilskii@gmail.com>
2023-04-24 14:23:16 +0200

    Changed Logging in Retrofit queries

--- app/src/main/java/com/safetyheads/akademiaandroida/AndroidAcademyApplication.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/AndroidAcademyApplication.kt
@@ -1,15 +1,13 @@
 package com.safetyheads.akademiaandroida
 
 import android.app.Application
-import android.util.Log
+import com.safetyheads.akademiaandroida.data.FirebaseConfigRepository
 import com.safetyheads.akademiaandroida.dropdownlist.DropDownListViewModel
 import com.safetyheads.akademiaandroida.dropdownlist.LoadItemsToDropDownListUseCase
-import com.safetyheads.domain.usecases.DelaySplashScreenUseCase
 import com.safetyheads.akademiaandroida.splashscreen.SplashScreenViewModel
 import com.safetyheads.data.network.retrofit.ApiClient
-import com.safetyheads.data.network.retrofit.AppLogger
-import com.safetyheads.akademiaandroida.data.FirebaseConfigRepository
 import com.safetyheads.domain.repositories.ConfigRepository
+import com.safetyheads.domain.usecases.DelaySplashScreenUseCase
 import com.safetyheads.domain.usecases.GetConfigUseCase
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -30,15 +28,7 @@ class AndroidAcademyApplication : Application() {
     }
 
     private val networkModule = module {
-        single<AppLogger> {
-            object : AppLogger {
-                override fun d(tag: String, message: String) {
-                    Log.d(tag, message)
-                }
-
-            }
-        }
-        single { ApiClient(BuildConfig.DEBUG, get()) }
+        single { ApiClient(BuildConfig.DEBUG) }
     }
 
     private val appModule = module {
--- data/src/main/java/com/safetyheads/data/network/retrofit/AppLogger.kt
+++
@@ -1,5 +0,0 @@
-package com.safetyheads.data.network.retrofit
-
-interface AppLogger {
-    fun d(tag: String, message: String)
-}
