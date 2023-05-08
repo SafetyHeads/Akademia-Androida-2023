c3a335e15a73bc0efffbf570caa2dfbc28d736b3
Rafal Krystek <rafal.krystek@outlook.com>
2023-05-08 13:45:22 +0200

    Created User Session Manager

--- app/src/main/java/com/safetyheads/akademiaandroida/AndroidAcademyApplication.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/AndroidAcademyApplication.kt
@@ -7,6 +7,11 @@ import com.safetyheads.akademiaandroida.dropdownlist.DropDownListViewModel
 import com.safetyheads.akademiaandroida.dropdownlist.LoadItemsToDropDownListUseCase
 import com.safetyheads.akademiaandroida.forgotpasswordfragment.ForgotPasswordViewModel
 import com.safetyheads.akademiaandroida.splashscreen.SplashScreenViewModel
+import com.safetyheads.akademiaandroida.usersessionmanager.*
+import com.safetyheads.akademiaandroida.usersessionmanager.FakeSessionGenerator
+import com.safetyheads.akademiaandroida.usersessionmanager.LoggedSessionManager
+import com.safetyheads.akademiaandroida.usersessionmanager.SESSION_SCOPE_NAME
+import com.safetyheads.akademiaandroida.usersessionmanager.UnloggedSessionManager
 import com.safetyheads.data.network.retrofit.ApiClient
 import com.safetyheads.domain.repositories.ConfigRepository
 import com.safetyheads.domain.repositories.UserRepository
@@ -17,6 +22,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 
@@ -27,7 +33,7 @@ class AndroidAcademyApplication : Application() {
         startKoin {
             androidLogger()
             androidContext(this@AndroidAcademyApplication)
-            modules(listOf(appModule, networkModule))
+            modules(listOf(appModule, networkModule, sessionModule))
         }
     }
 
@@ -50,5 +56,21 @@ class AndroidAcademyApplication : Application() {
         viewModel { SplashScreenViewModel(get(), get()) }
         viewModel { DropDownListViewModel(get()) }
         viewModel { ForgotPasswordViewModel(get()) }
+    }
+
+    private val sessionModule = module {
+        single { FakeSessionGenerator() }
+
+        scope(named(SESSION_SCOPE_NAME)) {
+            scoped {
+                val session = getSessionScope().getOrNull<Session>()
+                if (session == null) {
+                    UnloggedSessionManager(fakeSessionGenerator = get())
+                } else {
+                    LoggedSessionManager(session)
+                }
+            }
+        }
+
     }
 }
