40223fdd0a0f3ab8a3cddc33a904016f7b8a0523
Albert Bilski <albertbilskii@gmail.com>
2023-05-17 10:51:37 +0200

    create fragment_media.xml

--- presentation/src/test/java/com/safetyheads/presentation/EmailValidatorTest.kt
+++ presentation/src/test/java/com/safetyheads/presentation/EmailValidatorTest.kt
@@ -2,7 +2,7 @@ package com.safetyheads.akademiaandroida
 
 import android.content.Context
 import android.widget.EditText
-import com.safetyheads.presentation.utils.EmailValidator
+import com.safetyheads.akademiaandroida.presentation.ui.utils.EmailValidator
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
