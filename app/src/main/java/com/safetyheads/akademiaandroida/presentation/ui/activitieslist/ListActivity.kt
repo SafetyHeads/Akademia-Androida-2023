567ea4c28944d67a501a870c00d4c109fa119312
Zalezny <zaleznypl@gmail.com>
2023-05-17 18:58:43 +0200

    better size of back_arrow and avatar

--- app/src/main/java/com/safetyheads/akademiaandroida/presentation/ui/activitieslist/ListActivity.kt
+++ app/src/main/java/com/safetyheads/akademiaandroida/presentation/ui/activitieslist/ListActivity.kt
@@ -81,6 +81,7 @@ class ListActivity : AppCompatActivity() {
         forgotPasswordFragment,
         technolgyStackFragment,
         signUpFragment,
+        careerFragment,
         faqFragment
     )
 }
