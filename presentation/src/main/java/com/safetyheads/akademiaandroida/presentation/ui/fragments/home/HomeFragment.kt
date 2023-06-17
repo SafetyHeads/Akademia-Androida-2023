df1436d20d49c3a53470958d2170e876fc7e44af
Marcin Sareło <msarelo@safetyheads.com>
2023-06-17 21:28:49 +0200

    Fixes after merge

--- presentation/src/main/java/com/safetyheads/akademiaandroida/presentation/ui/fragments/home/HomeFragment.kt
+++ presentation/src/main/java/com/safetyheads/akademiaandroida/presentation/ui/fragments/home/HomeFragment.kt
@@ -7,19 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import com.safetyheads.akademiaandroida.presentation.databinding.FragmentDashboardBinding
+import com.safetyheads.akademiaandroida.presentation.databinding.FragmentHomeBinding
 import com.safetyheads.akademiaandroida.presentation.ui.viewmodels.DashboardViewModel
 
 class HomeFragment : Fragment() {
 
-    private lateinit var binding: FragmentDashboardBinding
+    private lateinit var binding: FragmentHomeBinding
     private val viewModel: DashboardViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
