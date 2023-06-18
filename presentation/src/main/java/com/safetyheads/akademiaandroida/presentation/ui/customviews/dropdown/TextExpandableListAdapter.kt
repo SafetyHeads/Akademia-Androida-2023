5836d6e7a79a88c46ff0d4ba6130b5231bf5527f
Kuba-Nalepa <nalepak11@gmail.com>
2023-06-18 22:43:15 +0200

    Fixed properly showing section Technology Stack data + little fixes in UI

--- presentation/src/main/java/com/safetyheads/akademiaandroida/presentation/ui/customviews/dropdown/TextExpandableListAdapter.kt
+++ presentation/src/main/java/com/safetyheads/akademiaandroida/presentation/ui/customviews/dropdown/TextExpandableListAdapter.kt
@@ -27,12 +27,11 @@ class TextExpandableListAdapter(
         convertView: View?,
         parent: ViewGroup
     ): View {
-        val parentInfo = getGroup(parentPosition)
-
-        return convertView ?: LayoutInflater.from(context)
-            .inflate(R.layout.dropdown_list_parent_item, null).apply {
-                findViewById<TextView>(R.id.dropdown_list_title).text = parentInfo.name
-            }
+        val view = LayoutInflater.from(context).inflate(R.layout.dropdown_list_parent_item, parent,false)
+        val listTitle = getGroup(parentPosition)
+        val listTitleTextView = view.findViewById<TextView>(R.id.dropdown_list_title)
+        listTitleTextView.text = listTitle.name
+        return view
     }
 
     override fun getChildView(
@@ -42,12 +41,11 @@ class TextExpandableListAdapter(
         convertView: View?,
         parent: ViewGroup
     ): View {
-        val childInfo = getChild(parentPosition, childPosition)
-
-        return convertView ?: LayoutInflater.from(context)
-            .inflate(R.layout.dropdown_list_child_item, null).apply {
-                findViewById<TextView>(R.id.itemList).text = childInfo.name
-            }
+        val view = LayoutInflater.from(context).inflate(R.layout.dropdown_list_child_item, parent,false)
+        val expandedListText = getChild(parentPosition, childPosition)
+        val expandedListTextView = view.findViewById<TextView>(R.id.itemList)
+        expandedListTextView.text = expandedListText.name
+        return view
     }
 
     override fun getGroupCount() = parentModels.size
